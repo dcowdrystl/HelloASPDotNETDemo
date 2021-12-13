ac6ed9ccf9ec33f1b3c95a97f036d743220dc455
Dan Cowdry <medtc411@gmail.com>
2021-12-13 07:11:42 -0800

    commit

---
+++ HelloASPDotNET/Controllers/BananaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class BananaController : Controller
+    {
+        [HttpGet]
+        
+        public IActionResult Index()
+        {
+            string html = "<h1>Nya is the time to choose a victim</h1>" +
+                "<form method='post' action='/banana/nya'>" +
+                "<select name='name'>" +
+                "<option value='Dan'>Your Teacher</option>" +
+                "<option value='Clark'>Not Your Teacher</option>" +
+                "</select>" +
+                "<input type='submit' value='Nya?'>" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        [HttpPost]
+        [Route("/banana/nya/{name?}")]
+        public IActionResult Nya(string name)
+        {
+            
+            return Content("<h1>Know, like nya. Right, " + name + "? </h1>", "text/html");
+        }
+    }
+}
---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    
+    public class HelloController : Controller
+    {
+        /*[HttpGet]
+        [Route("/helloworld/")]*/
+        public IActionResult Index()
+        {
+            string index = "<form method='POST' action='/hello/result'>" +
+                "The hello path<br>" + 
+                "<input type='submit' value='submit'><br>" +
+                "</form>";
+            return Content(index, "text/html");
+        }
+
+
+        //POST: /hello/
+        [HttpPost]
+        [Route("hello/result")]
+        public IActionResult FormSubmission()
+        {
+            string html = "Form Submitted!";
+            return Content(html, "text/html");
+        }
+    }
+}
